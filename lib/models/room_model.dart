9e6263252b3f9aaad24b6f59b834e9e2dd7a600f
TitanOfTime <immylance@gmail.com>
2026-02-15 13:08:10 +0530

    updated images

--- lib/models/room_model.dart
+++ lib/models/room_model.dart
@@ -41,7 +41,7 @@ final List<Room> mockRooms = [
     amenities: 'Projector, Whiteboard',
     capacity: 25,
     status: 'Booked',
-    imageUrl: 'https://images.unsplash.com/photo-1598327105666-5b89351aff97?w=600&h=400&fit=crop',
+    imageUrl: 'https://images.unsplash.com/photo-1497366216548-37526070297c?auto=format&fit=crop&w=800&q=80',
     location: '2nd Floor, East Wing',
   ),
   Room(
@@ -61,7 +61,7 @@ final List<Room> mockRooms = [
     amenities: 'Editing Workstation, Green Screen',
     capacity: 8,
     status: 'Available',
-    imageUrl: 'https://picsum.photos/600/400?random=4',
+    imageUrl: 'https://images.unsplash.com/photo-1758336716950-370f54b7a43f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNvdW5kcHJvb2Z8ZW58MHx8MHx8fDA%3D',
     location: '5th Floor, Media Center',
   ),
   Room(
